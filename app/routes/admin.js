ae1e4b7de429bf6de860cb48360f6c7e62830a1b
Kainan Gabriel <kainangabriel2019@gmail.com>
2021-03-10 20:54:01 -0300

    cadastro de categorias pelo db mongoose

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const mogoonse = require('mongoose')
+require('../models/Categoria')
+const Categoria = mogoonse.model('categorias')
 
 router.get('/', (req,res) => {
     res.render('admin/index')
@@ -17,5 +20,15 @@ router.get('/categorias/add', (req, res) =>{
     res.render('admin/addcategorias')
 })
 
+router.post('/categorias/nova', (req, res) => {
+    const novaCategoria  = {
+        nome: req.body.nome,
+        slug: req.body.slug
+    }
+
+    new Categoria(novaCategoria).save().then(() => {
+        console.log("Categoria salva com sucesso")
+    }).catch(error => console.log('error', error))
+})
 
 module.exports = router
